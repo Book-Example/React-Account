e8629e315d597cd28e6ce8481167019f4d19793b
iinsue <hinsu93@naver.com>
2022-03-18 16:18:43 +0900

    24.1.2 | 라우터 적용(721p)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {Routes, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+
 function App() {
   return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Routes>
+      <Route path='/*' element={<PostListPage/>} />
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/write' element={<WritePage/>}/>
+      <Route path='/@:username/:postId' element={<PostPage/>}/>
+    </Routes>
   );
 }
 
