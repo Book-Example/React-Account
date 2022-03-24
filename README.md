bab5a631cdc7d80dd2e3d1301f7dd5ad9ebf4d4b
iinsue <hinsu93@naver.com>
2022-03-24 12:40:42 +0900

    24.2.3 | API 연동하기(758p)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### npm install react-router-dom
 ### npm install styled-components
 ### npm install redux react-redux redux-actions immer redux-devtools-extension
+### npm install redux-saga
+### npm install axios redux-saga
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
-import rootReducer from './modules';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSage } from "./modules";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSage);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,5 +22,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
